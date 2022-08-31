5a220455db55c57c4fea166237f8f9f0986adb8d
Juan <juan.ospina@stonybrook.edu>
2022-08-31 19:10:32 -0400

    Updates

---
+++ precache-manifest.df6dc5298c3fc11c022e5361c1a4b7e0.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "cfc03f72ad4f1ea4f483",
+    "url": "/food-classification/static/css/main.9294ddce.chunk.css"
+  },
+  {
+    "revision": "cfc03f72ad4f1ea4f483",
+    "url": "/food-classification/static/js/main.cfc03f72.chunk.js"
+  },
+  {
+    "revision": "b9d8aaecb3beb8398829",
+    "url": "/food-classification/static/css/1.5d003680.chunk.css"
+  },
+  {
+    "revision": "b9d8aaecb3beb8398829",
+    "url": "/food-classification/static/js/1.b9d8aaec.chunk.js"
+  },
+  {
+    "revision": "e6e041d0f09c670f0980",
+    "url": "/food-classification/static/js/runtime~main.e6e041d0.js"
+  },
+  {
+    "revision": "3b53982c07bbc7a94a690240c38cbf90",
+    "url": "/food-classification/index.html"
+  }
+];
