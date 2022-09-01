dcdff63d8642ccd8a6e75cf5c629f1b2ab6ea260
jeo02 <70209456+jeo02@users.noreply.github.com>
2022-08-31 20:25:51 -0400

    Updates

---
+++ precache-manifest.9450071f8c315ec3fb3d72f296e6f96d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "0c4243096555063587ed",
+    "url": "/food-classification/static/css/main.9294ddce.chunk.css"
+  },
+  {
+    "revision": "0c4243096555063587ed",
+    "url": "/food-classification/static/js/main.0c424309.chunk.js"
+  },
+  {
+    "revision": "b9d8aaecb3beb8398829",
+    "url": "/food-classification/static/css/1.5d003680.chunk.css"
+  },
+  {
+    "revision": "b9d8aaecb3beb8398829",
+    "url": "/food-classification/static/js/1.b9d8aaec.chunk.js"
+  },
+  {
+    "revision": "e6e041d0f09c670f0980",
+    "url": "/food-classification/static/js/runtime~main.e6e041d0.js"
+  },
+  {
+    "revision": "05ada71b50e74cb7e61ef0499583bd8f",
+    "url": "/food-classification/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/food-classification/precache-manifest.f08aa9999840d6f46a0d23c9257c2077.js"
+  "/food-classification/precache-manifest.9450071f8c315ec3fb3d72f296e6f96d.js"
 );
 
 workbox.clientsClaim();
